db93f16e7f4c2cb39da0f85887efa7054b51f4b6
Joe Davitt <joseph.j.davitt@gmail.com>
2022-12-15 22:20:11 +0000

    build showCart

--- js/cart.js
+++ js/cart.js
@@ -18,17 +18,36 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  let cartItems = table.querySelectorAll('tbody > tr');
+  cartItems.forEach(e => e.remove());
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
-
+  let tableBody = table.querySelector('tbody');
   // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  let storedData = localStorage.getItem('cart');
+  let parsedData = JSON.parse(storedData);
+  for(let i = 0; i < parsedData.length; i++){
+    // TODO: Create a TR
+    let rowElem = document.createElement('tr');
+    tableBody.appendChild(rowElem);
+    // TODO: Create a TD for the delete link, quantity,  and the item
+    let xElem = document.createElement('td');
+    xElem.innerText = 'X';
+    rowElem.appendChild(xElem);
+    let quantElem = document.createElement('td');
+    quantElem.innerText = `${parsedData[i].quantity}`;
+    rowElem.appendChild(quantElem);
+    let tdElem = document.createElement('td');
+    tdElem.innerText = `${parsedData[i].product}`;
+    rowElem.appendChild(tdElem);
+    // TODO: Add the TR to the TBODY and each of the TD's to the TR
+
+  }
 
 }
 
